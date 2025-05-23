c9d5fb35fca14a2736eea720ea7d969354d796e8
nikita-collab <ilchenkon7728@gmail.com>
2025-05-23 14:41:52 +0200

    Add files via upload
    dorobilem to zadanie

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "login123";
+$database = "logowanie.db";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Błąd połączenia z bazą danych: " . $e->getMessage());
+}
+?>
